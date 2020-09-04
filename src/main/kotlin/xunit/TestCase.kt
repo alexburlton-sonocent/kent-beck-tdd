f4e240c4f5b4668b8d83677ebab301e893c4c442
Alex <alex.burlton@sonocent.com>
2020-09-04 16:43:04 +0100

    back to red. Awkward.

--- src/main/kotlin/xunit/TestCase.kt
+++ src/main/kotlin/xunit/TestCase.kt
@@ -11,6 +11,11 @@ open class TestCase(val name: String) {
             javaClass.getMethod(name).invoke(this)
         } catch (e: Exception) {
             result.testFailed()
+
+            val cause = e.cause
+            if (cause is AssertionError) {
+                throw cause
+            }
         }
 
         tearDown()
